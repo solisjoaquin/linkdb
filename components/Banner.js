53c7a65b61d318ced4d4f6888f873db5dabc2ffb
joaquin solis <joaquin.solis@devsar.com>
2021-08-26 21:25:17 -0300

    add article link in the banner

--- components/Banner.js
+++ components/Banner.js
@@ -16,7 +16,7 @@ export default function Example({ setDisplayBanner }) {
                     </div>
                     <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
                         <a
-                            href="#"
+                            href="https://joaquinsolis.hashnode.dev/create-an-app-with-auth0-nextjs-and-tailwind-from-development-to-production"
                             className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
                         >
                             Read article
